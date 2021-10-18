87b2c5d07f5090314c82a49f25881909ca3d5f4e
metafield <anthony.sylvester99@gmail.com>
2021-10-18 00:46:06 -0700

    init

---
+++ src/Aegis/Svg.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import type { SVGProps } from '../types';
+
+  export let props: SVGProps;
+</script>
+
+<svg
+  style={props.style}
+  width={props.width}
+  height={props.height}
+  xmlns="http://www.w3.org/2000/svg"
+  xmlns:xlink="http://www.w3.org/1999/xlink"
+>
+  <title>{props.title}</title>
+  <desc>{props.desc}</desc>
+  <slot />
+</svg>
+
+<style>
+  svg {
+    height: 100%;
+    width: 100%;
+  }
+</style>
---
+++ src/types.ts
@@ -0,0 +1,7 @@
+export interface SVGProps {
+  title: string;
+  desc: string;
+  width?: string;
+  height?: string;
+  style?: string;
+}
